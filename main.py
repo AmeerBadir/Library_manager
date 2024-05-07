d4d6e127ce82c3151f49d018becee34838e81954
AmeerBadir <100073193+AmeerBadir@users.noreply.github.com>
2024-05-08 01:45:33 +0300

    final

--- main.py
+++ main.py
@@ -2,6 +2,14 @@ from library import Library
 from book import Book
 
 
+def get_book_properties():
+    title = input("insert book title ")
+    author = input("insert book author ")
+    publication_year = input("insert book publication year ")
+    genre = input("insert book genre ")
+    return title, author, publication_year, genre
+
+
 def main():
     msg = ("what you want to do, \n(add) adds books to library, \n(del) delete book from library,\n(edit) to edit book "
            "\n(display) to display the library books, \n(q) to exit\n")
@@ -10,10 +18,7 @@ def main():
     my_library = Library()
     while inp != "q":
         if inp == "add":
-            title = input("insert book title ")
-            author = input("insert book author ")
-            publication_year = input("insert book publication year ")
-            genre = input("insert book genre ")
+            title, author, publication_year, genre = get_book_properties()
             try:
                 publication_year = int(publication_year)
                 book = Book(title, author, publication_year, genre)
@@ -23,16 +28,26 @@ def main():
                 inp = input(msg)
                 continue
         elif inp == "del":
-            title = input("insert book title ")
-            author = input("insert book author ")
-            publication_year = input("insert book publication year ")
-            genre = input("insert book genre ")
+            title, author, publication_year, genre = get_book_properties()
             try:
                 publication_year = int(publication_year)
                 book = Book(title, author, publication_year, genre)
                 my_library.delete_book(book)
             except:
                 print("deleting failed")
+                inp = input(msg)
+                continue
+        elif inp == "edit":
+            title, author, publication_year, genre = get_book_properties()
+            try:
+                book_to_edit = Book(title, author, publication_year, genre)
+                new_title = input("insert new title, if you dont want to change insert na ")
+                new_author = input("insert new author, if you dont want to change the author insert na ")
+                new_year = int(input("insert new year, if you dont want to change the year insert -1 "))
+                new_genre = input("insert new genre, if you dont want to change the genre  insert na ")
+                my_library.edit_book(book_to_edit, new_title, new_author, new_year, new_genre)
+            except:
+                print("editing failed")
                 inp = input(msg)
                 continue
         elif inp == "display":
@@ -52,9 +67,10 @@ def main():
                 inp = input(msg)
                 continue
 
-
         inp = input(msg)
 
 
 if __name__ == '__main__':
     main()
+
+
